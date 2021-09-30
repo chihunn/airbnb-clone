4d1ea717298a2123153d06ebf0386286933550fe
chihunn <sch804@pukyong.ac.kr>
2021-09-30 17:44:02 +0900

    Update Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,7 +6,10 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """Item Admin Definition"""
 
-    pass
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -14,7 +17,70 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Defnition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "Space",
+            {"fields": ("guests", "beds", "bedrooms", "baths")},
+        ),
+        (
+            "More About The Spaces",
+            {"fields": ("amenities", "facilities", "house_rules")},
+        ),
+        (
+            "Last Detail",
+            {"fields": ("host",)},
+        ),
+    )
+
+    list_display = (
+        "name",
+        "description",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+    search_field = ("=city", "^host_username")
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+    count_amenities.short_description = "Amenities"
 
 
 @admin.register(models.Photo)
