2f83a6bad7229850cf55a7b2e6a200dd2057db58
chihunn <sch804@pukyong.ac.kr>
2021-10-19 23:44:10 +0900

    Create List View

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-# Create your views here.
-def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+class HomeView(ListView):
+    "HomeView Definition"
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5
+    context_object_name = "rooms"
 
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-        },
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
